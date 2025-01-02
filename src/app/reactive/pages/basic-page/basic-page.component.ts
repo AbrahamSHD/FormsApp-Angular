f04a78c080d5419cd67b696791fa642e1c5d8893
Abraham HC <hernandez.c.abraham.00@gmail.com>
2025-01-01 23:32:35 -0600

    Practice project “FormsApp” started

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx = {
+  name: 'RTX 5090',
+  price: 10000,
+  inStorage: 50,
+}
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent implements OnInit {
+
+  // public myForm: FormGroup = new FormGroup ({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   storage: new FormControl(0),
+  // })
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3), Validators.maxLength(100) ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]],
+  })
+
+  constructor ( private fb: FormBuilder ) {}
+
+  ngOnInit(): void {
+    this.myForm.reset( rtx )
+  }
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field: string ): string | null {
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for ( const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracteres.`
+        case 'maxlength':
+          return `Máximo ${ errors['maxlength'].requiredLength } caracteres.`
+      }
+    }
+
+    return 'Hola Mundo'
+  }
+
+  onSave(): void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.myForm.value )
+
+    this.myForm.reset({ price: 10, inStorage: 10 })
+
+  }
+
+}
