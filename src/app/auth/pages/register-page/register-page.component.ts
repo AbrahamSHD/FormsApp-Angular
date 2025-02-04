89b8274d2a83bfc54797294d5d93a6eaa6fbddc9
Abraham HC <hernandez.c.abraham.00@gmail.com>
2025-02-04 06:08:59 -0600

    add: RegisterPage created

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: [ '', [ Validators.required ] ],
+    email: [ '', [ Validators.required ] ],
+    userName: [ '', [ Validators.required ] ],
+    password: [ '', [ Validators.required, Validators.minLength(6) ] ],
+    passwordTwo: [ '', [ Validators.required ] ],
+  })
+
+  constructor (
+    private fb: FormBuilder,
+  ) {}
+
+  onSubmit (  ) {
+
+    // TODO: Obtener la validación de un servicio
+
+  }
 
 }
